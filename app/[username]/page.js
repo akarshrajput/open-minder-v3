1bc25d0813c99e5f72e20cf156e67da96c0c6a60
Akarsh Rajput <112371948+akarshrajput@users.noreply.github.com>
2024-05-26 19:38:38 +0530

    Fix Bugs

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -5,7 +5,7 @@ const page = async ({ params }) => {
   const session = await auth();
   return (
     <div>
-      <p>Welcome {session.user.name}</p>
+      <p>Welcome {session?.user?.name}</p>
       <form action={signOutAction}>
         <button className="flex gap-1 items-center font-medium  bg-emerald-600 text-stone-50 py-2 px-2 rounded-[4px]">
           Logout
